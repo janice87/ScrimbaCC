29bc02b89ef116699a7e29a881fa5b0e2b3a0273
Janice Chan <janice.chan1288@gmail.com>
2023-02-28 11:32:41 -0500

    add solution

---
+++ 05.CandySale.js
@@ -0,0 +1,59 @@
+//import products from "./data.js";
+
+const products = [
+    {
+        item: "🍭",
+        price: 2.99,
+        type: "sweet",
+    },
+    {
+        item: "🍫",
+        price: 1.99, 
+        type: "sweet",
+    },
+    {
+        item: "🥫",
+        price: 1.99, 
+        type: "savory",
+    },
+    {
+        item: "🍬",
+        price: .89, 
+        type: "sweet",
+    },
+    {
+        item: "🥦",
+        price: 3.99,
+        type: "savory",
+    }, 
+    {
+        item: "🍖",
+        price: 3.99, 
+        type: "savory",
+    }, 
+]
+/*
+   It's the day after Halloween 🎃 and all the candy is on sale!
+   
+   To buy up all the candy, use map() and filter() to put all the
+   candy into a `shoppingCart` array. 
+   
+   The new array should contain only the item and the price, like
+   this: 
+   
+   Expected output: 
+   [
+       {item: "🍭", price: 2.99},
+       {item: "🍫", price: 1.99}, 
+       {item: "🍬", price: 0.89}
+    ]
+*/
+
+function getSaleItems(data){
+    return data.filter(item => item.type === "sweet").map(candy => {
+        return {item: candy.item, price: candy.price}
+    })
+   
+};
+
+console.log(getSaleItems(products))
