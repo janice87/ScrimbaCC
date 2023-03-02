3f9a517f5f95b064a89456e453e427a33cbfdfe8
Janice Chan <janice.chan1288@gmail.com>
2023-03-02 07:55:10 -0500

    add solution

---
+++ 07.AlternatingCaps.js
@@ -0,0 +1,27 @@
+/* Alternating Caps 
+ Write a function that takes in a string of letters
+ and returns a sentence in which every other letter is capitalized.
+
+Example input: "I'm so happy it's Monday"
+Example output: "I'M So hApPy iT'S MoNdAy"
+even #s are capitalized
+*/
+
+// using array
+function altCaps(str){
+    let newStr = str.split('')
+    let finalStr = []
+
+    for(let i = 0; i < newStr.length; i++){
+        if(i % 2 === 0){
+            finalStr.push(newStr[i].toUpperCase())            
+        } else {
+            finalStr.push(newStr[i])
+        }
+    }    
+    return finalStr.join('');
+}
+
+// using string concatenation
+
+console.log(altCaps("When you visit Portland you have to go to VooDoo Donuts"));
