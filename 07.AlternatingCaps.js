ef8f1224438899f52eddb370027c40ecd3f44ff3
Janice Chan <janice.chan1288@gmail.com>
2023-03-02 09:06:18 -0500

    added 2 solutions

--- 07.AlternatingCaps.js
+++ 07.AlternatingCaps.js
@@ -23,5 +23,16 @@ function altCaps(str){
 }
 
 // using string concatenation
+function altCaps(str){
+    let newStr = ''
+    for(let i = 0; i < str.length; i++){
+        if(i % 2 === 0){
+            newStr += str[i].toUpperCase()
+        } else {
+            newStr += str[i]
+        }
+    }
+    return newStr
+}
 
 console.log(altCaps("When you visit Portland you have to go to VooDoo Donuts"));
